436100ef34c10cba1184f08fa3e61680f7a1ffa4
Eduard <eduardvagin14@gmail.com>
2022-11-15 11:41:57 +0200

    modify activity

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlagin(activeSlide = 1) {
+    const slides = document.querySelectorAll('.slide');
 
-for(const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses();
+    slides[activeSlide - 1].classList.add('active');
 
-        slide.classList.add('active');
-    });
+    for(const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+
+            slide.classList.add('active');
+        });
+    }
+
+    function clearActiveClasses() {
+        slides.forEach(slide => {
+            slide.classList.remove('active');
+        });
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach(slide => {
-        slide.classList.remove('active');
-    });
-}
+slidesPlagin();
